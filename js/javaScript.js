886223e3a780519ba0aa20413d393fd6b7f9bdd3
PittnoiR <omargarza394@gmail.com>
2023-12-31 11:07:09 -0600

    clean code

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,24 +1,19 @@
-var uri = document.getElementById("Rl")
-if (uri) {
-    var idUrl = uri.id;
-    const url = 'https://url-shortener-service.p.rapidapi.com/shorten';
-    const options = {
-    	method: 'POST',
-    	headers: {
-    		'content-type': 'application/x-www-form-urlencoded',
-    		'X-RapidAPI-Key': '60f7e74316msh27534207aad635dp1490fejsnc78d61181d90',
-    		'X-RapidAPI-Host': 'url-shortener-service.p.rapidapi.com'
+const url = 'https://url-shortener-service.p.rapidapi.com/shorten';
+const options = {
+    method: 'POST',
+    headers: {
+	    	'content-type': 'application/x-www-form-urlencoded',
+	    	'X-RapidAPI-Key': '60f7e74316msh27534207aad635dp1490fejsnc78d61181d90',
+	    	'X-RapidAPI-Host': 'url-shortener-service.p.rapidapi.com'
     	},
-    	body: new URLSearchParams({
-    		url: 'https://www.youtube.com/watch?v=7-pnUgGurMU&list=RDEMpkUuvOoduuY0Cxxwh8iRMA&index=10',
-    	console.log('','url.href');
-    	})
-    };
+    body: new URLSearchParams({
+	    	url: 'https://www.youtube.com/watch?v=7-pnUgGurMU&list=RDEMpkUuvOoduuY0Cxxwh8iRMA&index=10'
+	    }),
+};
     
     fetch(url,options)
         .then(datos =>{
          console.log(datos)
         
         })
-        
-    }
+
